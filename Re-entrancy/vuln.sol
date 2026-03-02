77959ab6f53470917b296222bba05b20547a27b4
Duncan Kibet <92955713+Duncan254-u@users.noreply.github.com>
2026-03-02 06:36:53 +0300

    Implement withdrawal function with balance check
    Fix withdrawal function to check balance before sending funds.

--- Re-entrancy/vuln.sol
+++ Re-entrancy/vuln.sol
@@ -1,1 +1,5 @@
-
+function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) { // Check if the sender has enough to send)
+    msg.sender.call{value: _amount} (""); // Send the amount to the sender
+    balances{msg.sender} -=_amount;
+}
